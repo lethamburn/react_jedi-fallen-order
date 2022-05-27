2235f5f40a35f0a6daba37646537e3e34731089f
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-05-27 17:30:02 +0200

    FEAT Home

---
+++ src/layout/Pulse.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pulse = () => {
+  return (
+    <div className="pulse"></div>
+  )
+}
+
+export default Pulse
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Button from "../../layout/Button";
+import Pulse from "../../layout/Pulse";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main className="home">
+      <section className="home-header">
+        <img src="./images/logo.png" alt="Jedi Fallen Order Logo" />
+      </section>
+      <section className="home-hero">
+        <h2>Your Jedi journey</h2>
+        <p>
+          Aided by a former Jedi Knight, a prickly pilot and a fearless droid
+          companion, you must escape the sinister machinations of the Imperial
+          forces in this story driven adventure.
+        </p>
+      </section>
+      <section className="home-button">
+          <Button text="Explore the journey"/>
+      </section>
+      <section className="home-scroll">
+          <p>Scroll down</p>
+          <Pulse />
+      </section>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
