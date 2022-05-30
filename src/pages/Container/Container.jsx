f64b52ce883ace7faf6b059a6d6a95aff7936a3e
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-05-30 17:52:15 +0200

    REFACTOR video

--- src/pages/Container/Container.jsx
+++ src/pages/Container/Container.jsx
@@ -4,14 +4,17 @@ import Force from "../Force/Force";
 import GalaxyAwaits from "../GalaxyAwaits/GalaxyAwaits";
 import Kashyyyk from "../Kashyyyk/Kashyyyk";
 import Locations from "../Locations/Locations";
+import { Fade } from "react-awesome-reveal";
 const Container = () => {
-  const backgroundVideo = "./images/video.mp4"
+  const backgroundVideo = "./images/video.mp4";
   return (
     <>
       <div className="container">
-      <video autoPlay loop muted id="video">
-        <source src={backgroundVideo} type="video/mp4"/>
-      </video>
+        <Fade delay={200} triggerOnce>
+          <video autoPlay loop muted id="video">
+            <source src={backgroundVideo} type="video/mp4" />
+          </video>
+        </Fade>
         <Home />
         <Force />
         <GalaxyAwaits />
