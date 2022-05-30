b03f4f8b6a40e75bbafdb485978187471ae8ac80
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-05-30 17:46:48 +0200

    FEAT VIDEO BACKGROUND

--- src/pages/Container/Container.jsx
+++ src/pages/Container/Container.jsx
@@ -5,13 +5,19 @@ import GalaxyAwaits from "../GalaxyAwaits/GalaxyAwaits";
 import Kashyyyk from "../Kashyyyk/Kashyyyk";
 import Locations from "../Locations/Locations";
 const Container = () => {
+  const backgroundVideo = "./images/video.mp4"
   return (
     <>
-      <Home />
-      <Force/>
-      <GalaxyAwaits />
-      <Kashyyyk />
-      <Locations />
+      <div className="container">
+      <video autoPlay loop muted id="video">
+        <source src={backgroundVideo} type="video/mp4"/>
+      </video>
+        <Home />
+        <Force />
+        <GalaxyAwaits />
+        <Kashyyyk />
+        <Locations />
+      </div>
     </>
   );
 };
--- src/pages/Locations/Locations.jsx
+++ src/pages/Locations/Locations.jsx
@@ -5,47 +5,40 @@ import { Fade } from "react-awesome-reveal";
 const Locations = () => {
   const data = [
     {
-      image:
-        "./images/locations/bogano.jpg",
+      image: "./images/locations/bogano.jpg",
       caption: "Bogano",
     },
     {
-      image:
-        "./images/locations/zeffo.jpg",
+      image: "./images/locations/zeffo.jpg",
       caption: "Zeffo",
     },
     {
-      image:
-        "./images/locations/kashyyyk.jpg",
+      image: "./images/locations/kashyyyk.jpg",
       caption: "Kashyyyk",
     },
     {
-      image:
-        "./images/locations/dathomir.jpg",
+      image: "./images/locations/dathomir.jpg",
       caption: "Dathomir",
     },
     {
-      image:
-        "./images/locations/bracca.jpg",
+      image: "./images/locations/bracca.jpg",
       caption: "Bracca",
     },
     {
-      image:
-        "./images/locations/ilum.jpg",
+      image: "./images/locations/ilum.jpg",
       caption: "Ilum",
     },
     {
-      image:
-        "./images/locations/nur.jpg",
+      image: "./images/locations/nur.jpg",
       caption: "Nur",
-    }
+    },
   ];
 
   const captionStyle = {
     fontSize: "2em",
     fontWeight: "bold",
     fontFamily: "Gothic",
-    textTransform: "uppercase"
+    textTransform: "uppercase",
   };
   const slideNumberStyle = {
     fontSize: "20px",
@@ -53,42 +46,44 @@ const Locations = () => {
   };
   return (
     <Fade delay={300}>
-    <section className="locations" id="locations">
-    <article>
-        <h2>Locations</h2>
-        <h3>Locations</h3>
-    </article>
-      <article>
-        <Carousel
-          data={data}
-          time={2000}
-          width="800px"
-          height="400px"
-          captionStyle={captionStyle}
-          radius="30px"
-          slideNumber={false}
-          slideNumberStyle={slideNumberStyle}
-          captionPosition="bottom"
-          automatic={true}
-          dots={true}
-          pauseIconColor="white"
-          pauseIconSize="40px"
-          slideBackgroundColor="darkgrey"
-          slideImageFit="cover"
-          thumbnails={true}
-          thumbnailWidth="100px"
-          style={{
-            textAlign: "center",
-            maxWidth: "850px",
-            maxHeight: "500px",
-            margin: "40px auto"
-          }}
-        />
-      </article>
-      <article className="scroll">
-          <Pulse />
-        </article>
-    </section>
+      <section className="locations" id="locations">
+        <Fade cascade>
+          <article>
+            <h2>Locations</h2>
+            <h3>Locations</h3>
+          </article>
+          <article>
+            <Carousel
+              data={data}
+              time={2000}
+              width="800px"
+              height="400px"
+              captionStyle={captionStyle}
+              radius="30px"
+              slideNumber={false}
+              slideNumberStyle={slideNumberStyle}
+              captionPosition="bottom"
+              automatic={true}
+              dots={true}
+              pauseIconColor="white"
+              pauseIconSize="40px"
+              slideBackgroundColor="darkgrey"
+              slideImageFit="cover"
+              thumbnails={true}
+              thumbnailWidth="100px"
+              style={{
+                textAlign: "center",
+                maxWidth: "850px",
+                maxHeight: "500px",
+                margin: "40px auto",
+              }}
+            />
+          </article>
+          <article className="scroll">
+            <Pulse />
+          </article>
+        </Fade>
+      </section>
     </Fade>
   );
 };
